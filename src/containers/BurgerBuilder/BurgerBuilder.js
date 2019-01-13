328c5364e3f035e30cce183943d9fe76bfb84705
Imran105 <sikder.5.imran@gmail.com>
2019-01-13 20:13:19 +0600

    Initial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/aux';
+
+
+class BurgerBuilder extends Component{
+
+       render(){
+
+          return(
+
+              <Aux>
+                   <div>Burger</div>
+                   <div>BurgerBuilder</div>
+              </Aux>
+          );
+
+       }
+
+}
+
+export default BurgerBuilder;
